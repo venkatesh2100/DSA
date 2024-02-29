ea029e4efe87ee422c8f2415299e192069a20f29
venkatesh2100 <119948075+venkatesh2100@users.noreply.github.com>
2024-02-29 16:33:05 +0530

    Updated Readme

--- DSA/STRINGS/demo.java
+++ DSA/STRINGS/demo.java
@@ -2,8 +2,9 @@ package DSA.STRINGS;
 
 public class demo {
   public static void main(String[] args) {
-    String name="venky";
-    System.out.println("Hi "+name);
+    String name = "venky";
+    int age = 19;
+    System.out.println("Hi " + name+" and your'e "+age);
   }
-  
+
 }
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # DSA
 🎯 Welcome to DSA Repository 🎯         This repository covers basic topics related to data structures and algorithms (DSA).
+
+Hi
